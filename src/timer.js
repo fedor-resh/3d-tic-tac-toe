e28a3d55b49de859d1b8de1fb08b143f48ed86f0
fedor-resh <fedor_20052005@mail.ru>
2023-06-24 18:55:27 +0300

    update timer

--- src/timer.js
+++ src/timer.js
@@ -1,15 +1,21 @@
 export class Timer {
-    timer = null;
-    constructor() {
-        this.start = Date.now();
-    }
-    startTimer() {
-
+    seconds = 120
+    interval = null
+    constructor(callback) {
+        callback(Timer.getClock(this.seconds))
+        this.interval = setInterval(()=>{
+            callback(Timer.getClock(this.seconds))
+            this.seconds--
+        }, 1000)
     }
     stopTimer() {
-
+        clearInterval(this.interval)
     }
-    everySecond(callback) {
-        callback(this.timer)
+    static getClock(seconds) {
+        let minutes = Math.floor(seconds / 60)
+        let seconds_ = seconds % 60
+        if (seconds_ < 10)
+            seconds_ = '0' + seconds_
+        return `${minutes}:${seconds_}`
     }
 }
